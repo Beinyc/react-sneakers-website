1b69bfe6246e54053c102b0e448206e9f60de1ab
Beiny <apatenko.nikita31rus@yandex.ru>
2023-11-05 22:19:06 +0300

    сделан рендеринг кроссовок через  массив

--- src/App.js
+++ src/App.js
@@ -4,6 +4,29 @@ import Header from './components/Header/Header'
 import Overlay from './components/Overlay/Overlay';
 import Main from './components/Main/Main';
 
+const sneakers = [
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 12999,
+    imageUrl: '/img/sneakers/sneakers 1.jpg',
+  },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: 12999,
+    imageUrl: '/img/sneakers/sneakers 2.jpg',
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 8499,
+    imageUrl: '/img/sneakers/sneakers 3.jpg',
+  },
+  {
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: 8999,
+    imageUrl: '/img/sneakers/sneakers 4.jpg',
+  },
+]
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -11,8 +34,11 @@ function App() {
       <Overlay/>
       <Main/>
       <div className='card-flex'>
-        <Card/>
-        <Card/>
+        {
+          sneakers.map((obj) => (
+            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+          ))
+        }
       </div>    
     </div>
   );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import './Card.css'
 
-export default function Card() {
+export default function Card(props) {
   return (
       <div className='card__favorite'>
         <div className='card__favorites'>
           <img src='/img/heard-unliked.svg' alt='unliked'/>
         </div>
-          <img width={133} height={112} src='./img/sneakers/sneakers 1.jpg'alt='кроссовки'/>
-          <h5 className='card__title'>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+          <img width={133} height={112} src={props.imageUrl} alt='кроссовки'/>
+          <h5 className='card__title'>{props.title}</h5>
           <div className='card__button'>
           <div className='card__full-price'>
               <div className='card__total-price'>
                 <span className='card_prise'>Цена:</span>
-                <b className='card__prises'>12 999 руб.</b>
+                <b className='card__prises'>{props.price} руб.</b>
               </div>
           </div>
             <button className='button'>

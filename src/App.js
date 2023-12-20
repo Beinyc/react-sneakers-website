162e77d1af8f8715acfb071af11ba1206ab69348
Beiny <apatenko.nikita31rus@yandex.ru>
2023-12-20 14:49:39 +0300

    Updating the shopping cart
    Реализовано :
    Добавление в корзину
    Удаление из корзины
    Связка с бэкендом:
    Добавление и удаление на стороне сервера

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 //Компоненты
 import Card from './components/Card/Card'
@@ -17,17 +18,22 @@ function App() {
   const [ cartOpened, setCartOpened ] = useState(false)
   
   React.useEffect(() => {
-  fetch('https://655b4fb0ab37729791a8e51d.mockapi.io/items')
-  .then((res) => {
-    return res.json();
-  })
-  .then((json) => {
-    setItems(json);
-  })
+  axios.get('https://655b4fb0ab37729791a8e51d.mockapi.io/items').then(res =>{
+    setItems(res.data)
+  });
+  axios.get('https://655b4fb0ab37729791a8e51d.mockapi.io/cart').then(res =>{
+    setCartItems(res.data)
+  });
  }, []);
 
  const onAddToCart = (obj) => {
+  axios.post('https://655b4fb0ab37729791a8e51d.mockapi.io/cart', obj)
   setCartItems([... cartItems, obj]);
+ }
+
+ const onRemoveItem = ( id ) => {
+  axios.delete(`https://655b4fb0ab37729791a8e51d.mockapi.io/cart/${id}`)
+  setCartItems((cartItems) => cartItems.filter(item => item.id !== id));
  }
 
  const onCgangeSearchInput = (event) => {
@@ -36,7 +42,7 @@ function App() {
 
   return (
     <div className="wrapper clear">
-      {cartOpened && <Overlay items={cartItems} onClose={() => setCartOpened(false)}/>}
+      {cartOpened && <Overlay items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem}/>}
       <Header onClickCart={() => setCartOpened(true)}/>
       <Main onCgangeSearchInput={onCgangeSearchInput} searchValue={searchValue}/>
       <div className='card-flex'>
--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -3,42 +3,60 @@ import './Overlay.css'
 
 import btndelete from '../../images/корзина.svg'
 
-export default function Overlay({ onClose, items=[] }) {
+export default function Overlay({ onClose, onRemove ,items=[] }) {
   return (
     <div className='overlay'>
        <div className='overlay__drawer'>
         <div className='overlay__wraper'>
-          <h2 className='draver__title'>Корзина</h2>
-          <img onClick={onClose} className='delteBtn' src={btndelete} alt='delete-button'/>
-        </div>
-        <div className='overlay__cart-btn'>
+          <div className='overlay__wraper-top'>
+            <h2 className='draver__title'>Корзина</h2>
+            <img onClick={onClose} className='delteBtn' src={btndelete} alt='delete-button'/>
+          </div>
+
           {
-            items.map((obj) => (
-              <div className='overlay__cart-item'>
-              <div className='overlay__image' style={{backgroundImage: `url(${obj.imageUrl})`}}></div>
-            {/* <img className='overlay__image' src='./img/sneakers/sneakers 1.jpg'  width={70} height={70}/> */}
-            <div className='overlay__zone'>
-              <p className='overlay__zone-text'>{obj.title}</p>
-              <b className='overlay__price'>{obj.price}руб.</b>
+            items.length > 0 ? (
+            <div>
+              <div className='overlay__cart-btn'>
+            {
+              items.map((obj) => (
+                <div className='overlay__cart-item'>
+                <div className='overlay__image' style={{backgroundImage: `url(${obj.imageUrl})`}}></div>
+              {/* <img className='overlay__image' src='./img/sneakers/sneakers 1.jpg'  width={70} height={70}/> */}
+              <div className='overlay__zone'>
+                <p className='overlay__zone-text'>{obj.title}</p>
+                <b className='overlay__price'>{obj.price}руб.</b>
+              </div>
+                <img onClick={()=> onRemove(obj.id)}  className='delteBtn' src={btndelete} alt='delete-button'/>
+          </div>
+              ))
+            }
+          </div>
+          <ul className='overlay__total-block'>
+          <li className='overlay__total-list'>
+            <span>Итого</span>
+            <div className='overlay__total-image'></div>
+            <b>21 498 руб. </b>
+          </li>
+          <li className='overlay__total-list'>
+            <span>Итого</span>
+            <div className='overlay__total-image'></div>
+            <b>21 498 руб. </b>
+          </li>
+        </ul>
+        <button className='cartButton'>Оформить заказ</button>
             </div>
-              <img className='delteBtn' src={btndelete} alt='delete-button'/>
-        </div>
-            ))
+          ) : (<div className='cartEmpty'>
+            <img width='120px' height='120px' src='/img/empty-cart.jpg' alt='картинка корзина пуста'/>
+            <h2 className='drawer__empty-tittle'>Корзина пустая</h2>
+            <p className='drawer__empty-paragraph'>Добавьте хотя бы одну пару кроссовок, что бы сделать заказ.</p>
+            <button className='greenButton' onClick={onClose}>
+              <img className='drawer__empty-tittle_button' src='/img/buttons/arrow.svg' alt='Arrow'/>
+               Вернуться назад
+            </button>
+          </div>)
           }
-        </div>
-        <ul className='overlay__total-block'>
-            <li className='overlay__total-list'>
-              <span>Итого</span>
-              <div className='overlay__total-image'></div>
-              <b>21 498 руб. </b>
-            </li>
-            <li className='overlay__total-list'>
-              <span>Итого</span>
-              <div className='overlay__total-image'></div>
-              <b>21 498 руб. </b>
-            </li>
-          </ul>
-          <button className='cartButton'>Оформить заказ</button>
+          
+          </div>
        </div>
       </div>
   )
